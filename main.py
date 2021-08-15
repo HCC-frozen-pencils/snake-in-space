2fc9a0ff41c20d2d32af6f22b1271a7ecfa198c3
caith <>
2021-08-15 15:41:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import turtle
+
+t = turtle.Turtle()
+
+# To make it easier to see the turtle, let's make it a shape!
+t.shape('turtle')
+t.color('green')
+
+# Let's brighten things up! We can change the background of the screen
+background = t.getscreen()
+background.bgcolor("light blue")
+
+# grinning face
+print("\U00002744", "\U0000270F")
+print("I like to draw and eat frozen fruit!", "\U0001f600")
+
+#screen = turtle.Screen()
+#screen.register_shape('car.gif')
+
+#t = turtle.Turtle()
+#t.shape('car.gif')
